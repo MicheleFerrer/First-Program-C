79f5122da8cc53b871724869f7be9a27446a8d86
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-17 07:20:34 -0400

    playing with escape sequences

--- FirstCprogram.c
+++ FirstCprogram.c
@@ -4,5 +4,6 @@ int main()
 {
     printf("hello world! \n");
     printf("I created a new second line under the statement hello world using backslash n");
+    printf("hi \"hi world \\sup\n \t hey");
     return 0;
 }
