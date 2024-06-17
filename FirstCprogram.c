0a8e46c55a81879f4c74f061ceb2ef0531e16234
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-17 08:03:02 -0400

    format specifiers

--- FirstCprogram.c
+++ FirstCprogram.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int main()
+/*int main()
 {
     printf("hello world! \n");
     printf("I created a new second line under the statement hello world using backslash n");
     printf("hi \"hi world \\sup\n \t hey");
     return 0;
+}*/
+
+int main( ){
+  int myNumber = 50;
+printf("%d", myNumber);
+return 0;  
 }
