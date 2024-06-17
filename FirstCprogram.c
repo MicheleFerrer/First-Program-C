6aa5119b054e94f70f500395a001d64931809737
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-17 07:11:46 -0400

    ;

--- FirstCprogram.c
+++ FirstCprogram.c
@@ -3,6 +3,6 @@
 int main()
 {
     printf("hello world! \n");
-    printf("I created a new second line under the statement hello world using backslash n")
+    printf("I created a new second line under the statement hello world using backslash n");
     return 0;
 }
