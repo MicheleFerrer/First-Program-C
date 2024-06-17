9092ed5bea5cd0803b406f0ce87f1b79dee8c615
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-17 07:10:31 -0400

    Creating new second line

--- FirstCprogram.c
+++ FirstCprogram.c
@@ -2,6 +2,7 @@
 
 int main()
 {
-    printf("hello world!");
+    printf("hello world! \n");
+    printf("I created a new second line under the statement hello world using backslash n")
     return 0;
 }
